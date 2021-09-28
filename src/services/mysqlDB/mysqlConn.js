a91d00a55583fa37ed8c870c84cd1bb44f224e1c
Jesser Lemus <ijesser@outlook.com>
2021-09-27 20:34:43 -0500

    Establish mysql2/promise database connection

--- src/services/mysqlDB/mysqlConn.js
+++ src/services/mysqlDB/mysqlConn.js
@@ -1,18 +1,11 @@
-import mysql from "mysql";
+import mysql from "mysql2/promise";
 import config from "../../config/default.js";
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: config.MYSQL_HOST,
     user: config.MYSQL_USER,
     password: config.MYSQL_PASSWORD,
-    database: config.MYSQL_DATABASE
+    database: config.MYSQL_DATABASE,
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error("error mysql:" + err.stack);
-        return;
-    }
-});
-
-export default connection;
+export default pool;
