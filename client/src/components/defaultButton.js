f195e25e85fc7848bb44c8f3a73e153a55a16887
joseph <joseph.2210@hotmail.es>
2021-09-30 15:25:58 -0500

    delete files React

---
+++ client/src/components/defaultButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const defaultButton = () => (
+    <Button>
+        wenas
+    </Button>
+)
+
+export default defaultButton
---
+++ client/src/pages/home.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from "react";
+
+const home = () =>(
+    <Fragment>
+        
+    </Fragment>
+)
+
+export default home
