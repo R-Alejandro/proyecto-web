8db2ba975001843c9e1cd17b34fc1019cf9def62
joseph <joseph.2210@hotmail.es>
2021-09-30 21:42:38 -0500

    Form format

--- client/src/pages/singUp.js
+++ client/src/pages/singUp.js
@@ -1,32 +1,52 @@
 import React from "react"
+import Input from "../components/input"
+import './../components/styles/style_singUp.css'
 
-class singUp extends React.Component{
+import nameIcon from './../images/singUp_name.svg'
+import emailIcon from './../images/singUp_email.svg'
+import nickNameIcon from './../images/singUp_nickName.svg'
+import passwordIcon from './../images/singUp_password.svg'
+
+class singUp extends React.Component {
     render() {
         return (
-            <div>
-                <form>
-                    <label>
-                        Nombre
-                        <input type="text" name="name"/>
-                    </label>
-                    <label>
-                        Email
-                        <input type="text" name="email"/>
-                    </label>
-                    <label>
-                        Nickname
-                        <input type="text" name="Nickname"/>
-                    </label>
-                    <label>
-                        password
-                        <input type="password" name="password"/>
-                    </label>
-                    <label>
-                        check
-                        <input type="checkbox" name="check"/>
-                    </label>
-                    <input type="submit" value="Submit"/>
-                </form>
+            <div className="singUp__AllContainer">
+                <div>
+
+                </div>
+                <div className="allContainer__form">
+                    <form>
+                        <Input
+                            icon = {nameIcon}
+                            label = "Nombre"
+                            type = "text"
+                            name = "name"
+                        />
+                        <Input
+                            icon = {emailIcon}
+                            label = "Email"
+                            type = "text"
+                            name = "email"
+                        />
+                        <Input
+                            icon = {nickNameIcon}
+                            label = "Nickname"
+                            type = "text"
+                            name = "Nickname"
+                        />
+                        <Input
+                            icon = {passwordIcon}
+                            label = "Password"
+                            type = "password"
+                            name = "password"
+                        />
+                        <label className="form__checkbox">
+                            <input type="checkbox" name="check" />
+                            check
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
         )
     }
