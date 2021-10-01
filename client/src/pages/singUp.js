631220f25c8310ce03c0babe7991b0bac4ef5569
joseph <joseph.2210@hotmail.es>
2021-09-30 20:01:33 -0500

    form SingUp

--- client/src/pages/singUp.js
+++ client/src/pages/singUp.js
@@ -4,7 +4,29 @@ class singUp extends React.Component{
     render() {
         return (
             <div>
-                singUp
+                <form>
+                    <label>
+                        Nombre
+                        <input type="text" name="name"/>
+                    </label>
+                    <label>
+                        Email
+                        <input type="text" name="email"/>
+                    </label>
+                    <label>
+                        Nickname
+                        <input type="text" name="Nickname"/>
+                    </label>
+                    <label>
+                        password
+                        <input type="password" name="password"/>
+                    </label>
+                    <label>
+                        check
+                        <input type="checkbox" name="check"/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         )
     }
