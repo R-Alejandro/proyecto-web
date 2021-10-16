f71756f26ba2a7204718fc82733ffa565faf11f6
Jesser Lemus <ijesser@outlook.com>
2021-10-15 21:26:01 -0500

    Add dashboard general route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import users from "../api/users/routes.js";
+import dashboards from "../api/dashboard/router.js";
+
 import auth from "./auth.js";
 
 const router = Router();
 
 //write down here the routes needed, this is a general router
 router.use('/users', users);
+router.use('/dashboards', dashboards);
 router.use('/auth', auth);
 
 export default router;
